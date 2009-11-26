da3f49eb690f4095353037e1f119becde5f9439e
Dario Freddi <drf@kde.org>
2009-11-26 13:36:28 +0000

    Fix i18n catalog name
    svn path=/trunk/kdereview/polkit-kde/manager/; revision=1054670

--- Messages.sh
+++ Messages.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 $EXTRACTRC `find -name \*.ui -o -name \*.rc -o -name \*.kcfg` >> rc.cpp || exit 11
-$XGETTEXT `find -name \*.cpp -o -name \*.h` -o $podir/policykit-kde.pot
+$XGETTEXT `find -name \*.cpp -o -name \*.h` -o $podir/polkit-kde-authentication-agent-1.pot
 rm -f rc.cpp
