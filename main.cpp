8a2137643299f9647d5b683c0b564cf73defc36b
Andreas Hartmetz <ahartmetz@gmail.com>
2015-07-02 17:55:33 +0200

    Revert "Fix warning"
    This reverts commit 33a17014baf4f7f96e9fdeb67bd995412de02f59.
    
    It made klauncher unable do recognize that the agent started
    successfully, which stalled the whole startup process and caused a
    slow and limited "emergency start" without session restore, autostart
    and other nice things.
    Now I don't know how to change all the names involved to make
    klauncher happy again *and* have good names for everything, too,
    so I'm just fixing the most important problem.
    
    CCMAIL: aleixpol@kde.org

--- main.cpp
+++ main.cpp
@@ -42,7 +42,6 @@ int main(int argc, char *argv[])
     KAboutData::setApplicationData(aboutData);
 
     QCoreApplication::setOrganizationDomain(QStringLiteral("kde.org"));
-    QCoreApplication::setApplicationName("polkit.kde.authentication.agent1");
     QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps, true);
 
     KCrash::setFlags(KCrash::AutoRestart);
