9240bc3f562545aa7e6b06db1fa34dc78cf073c4
l10n daemon script <scripty@kde.org>
2015-09-22 12:25:38 +0000

    SVN_SILENT made messages (.desktop file) - always resolve ours
    In case of conflict in i18n, keep the version of the branch "ours"
    To resolve a particular conflict, "git checkout --ours path/to/file.desktop"

--- polkit-kde-authentication-agent-1.desktop.cmake
+++ polkit-kde-authentication-agent-1.desktop.cmake
@@ -1,6 +1,20 @@
 [Desktop Entry]
 Name=PolicyKit Authentication Agent
+Name[ca]=Agent d'autenticació del PolicyKit
+Name[es]=Agente de autenticación de PolicyKit
+Name[nl]=Authenticatieagent van PolicyKit
+Name[pt]=Agente de Autenticação do PolicyKit
+Name[pt_BR]=Agente de Autenticação do PolicyKit
+Name[uk]=Агент розпізнавання PolicyKit
+Name[x-test]=xxPolicyKit Authentication Agentxx
 Comment=PolicyKit Authentication Agent
+Comment[ca]=Agent d'autenticació del PolicyKit
+Comment[es]=Agente de autenticación de PolicyKit
+Comment[nl]=Authenticatieagent van PolicyKit
+Comment[pt]=Agente de Autenticação do PolicyKit
+Comment[pt_BR]=Agente de Autenticação do PolicyKit
+Comment[uk]=Агент розпізнавання PolicyKit
+Comment[x-test]=xxPolicyKit Authentication Agentxx
 Exec=${CMAKE_INSTALL_FULL_LIBEXECDIR}/polkit-kde-authentication-agent-1
 Terminal=false
 Type=Application
