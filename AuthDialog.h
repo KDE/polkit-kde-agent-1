0896b9158489a789db827901b3f68dda3dbe3755
Alexander Lohnau <alexander.lohnau@gmx.de>
2021-05-22 09:24:23 +0200

    Convert license headers to SPDX

--- AuthDialog.h
+++ AuthDialog.h
@@ -1,23 +1,9 @@
 /*  This file is part of the KDE project
-    Copyright (C) 2007-2008 Gökçen Eraslan <gokcen@pardus.org.tr>
-    Copyright (C) 2008 Daniel Nicoletti <dantti85-pk@yahoo.com.br>
-    Copyright (C) 2010 Dario Freddi <drf@kde.org>
+    SPDX-FileCopyrightText: 2007-2008 Gökçen Eraslan <gokcen@pardus.org.tr>
+    SPDX-FileCopyrightText: 2008 Daniel Nicoletti <dantti85-pk@yahoo.com.br>
+    SPDX-FileCopyrightText: 2010 Dario Freddi <drf@kde.org>
 
-    This program is free software; you can redistribute it and/or
-    modify it under the terms of the GNU General Public
-    License as published by the Free Software Foundation; either
-    version 2 of the License, or (at your option) any later version.
-
-    This library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Library General Public License for more details.
-
-    You should have received a copy of the GNU Library General Public License
-    along with this library; see the file COPYING.LIB.  If not, write to
-    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-    Boston, MA 02110-1301, USA.
-
+    SPDX-License-Identifier: GPL-2.0-or-later
 */
 
 #ifndef AUTHDIALOG_H
