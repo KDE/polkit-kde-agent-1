418b338196b8f57e28c4121d6bab6dd5cfefee39
Victor Ryzhykh <victorr2007@yandex.ru>
2024-01-24 17:06:33 +0000

    Added processing of qml files

--- Messages.sh
+++ Messages.sh
@@ -4,5 +4,5 @@
 # SPDX-FileCopyrightText: none
 
 $EXTRACTRC `find -name \*.ui -o -name \*.rc -o -name \*.kcfg` >> rc.cpp || exit 11
-$XGETTEXT `find -name \*.cpp -o -name \*.h` -o $podir/polkit-kde-authentication-agent-1.pot
+$XGETTEXT `find -name \*.cpp -o -name \*.h  -o -name \*.qml` -o $podir/polkit-kde-authentication-agent-1.pot
 rm -f rc.cpp
